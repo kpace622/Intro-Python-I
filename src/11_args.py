ecd2e39432a1adfe2c1eb9f5c05957d921ba596f
Kyle Pace <kpace2296@gmail.com>
2020-09-02 21:51:39 -0500

    finished 11 out of 15

--- src/11_args.py
+++ src/11_args.py
@@ -5,7 +5,8 @@
 # the sum. This is what you'd consider to be a regular, normal function.
 
 # YOUR CODE HERE
-
+def f1(arg1, arg2):
+    return arg1 + arg2
 print(f1(1, 2))
 
 # Write a function f2 that takes any number of integer arguments and returns the
@@ -13,7 +14,9 @@ print(f1(1, 2))
 # Note: Google for "python arbitrary arguments" and look for "*args"
 
 # YOUR CODE HERE
-
+def f2(*args):
+    return sum(args)
+    
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
 print(f2(1, 4, -12))            # Should print -7
